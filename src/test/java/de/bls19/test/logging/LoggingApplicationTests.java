a5e40388c014f88725aa40360b81c148d2756c9b
Oliver Wagner <Oliver.Wagner@vsa.de>
2021-02-22 14:49:01 +0100

    initial import from springboot starter

---
+++ src/test/java/de/bls19/test/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bls19.test.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
